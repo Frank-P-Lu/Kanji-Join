f2da30fd1c12beac9f3d562af4513710ea3a9060
frank <franklu.99@outlook.com>
2020-05-03 08:11:04 +1000

    Made gradle project.

--- src/main/java/game/components/Board.java
+++ src/main/java/game/components/Board.java
@@ -2,12 +2,12 @@ package game.components;
 
 import game.components.kanji.Kanji;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /*
 Essentially a chess board where radicals can be popped on.
-Co-ordinates: [x, y].
+Co-ordinates: [x, y]. These are not zero indexed.
 Eventually, these should be mouse driven.
  [
     [(1,1), (2,1)],
@@ -15,20 +15,64 @@ Eventually, these should be mouse driven.
 ]
  */
 public class Board {
-    private int length;
-    private int width;
-    private Map<Integer, List<Kanji>> board;  // rows and columns
+    private final int numberColumns;
+    private final int numberRows;
+    private List<List<Kanji>> board;  // rows and columns
 
 
-    public Board(int length, int width) {
-        this.length = length;
-        this.width = width;
+    public Board(int numberColumns, int numberRows) {
+        this.numberColumns = numberColumns;
+        this.numberRows = numberRows;
+        initBoard();
     }
 
-    public void addKanji(int insertX, int insertY, Kanji kanji){
+    private void initBoard(){
+        this.board = new ArrayList<>(numberRows);  // number of columns
+        for (int column = 0; column < numberRows; column++ ){
+            this.board.add(new ArrayList<>(numberColumns));
+        }
+    }
 
+    private Coordinate processCoordinates(int column, int row) throws InvalidCoordinateException{
+        Coordinate coordinate = new Coordinate(column, row);
+        coordinate.standardiseIndices();
+        if(!isValidCoordinate(coordinate)){
+            throw new InvalidCoordinateException();
+        }
+        return coordinate;
+    }
+
+    private boolean isValidCoordinate(Coordinate coordinate) {
+        return (coordinate.row < this.numberRows) && (coordinate.row > -1)
+                && (coordinate.column < numberColumns) && (coordinate.column > -1) ;
     }
 
 
+    public void addKanji(int column, int row, Kanji kanji) throws InvalidCoordinateException {
+        Coordinate coordinate = processCoordinates(column, row);
+        this.board.get(coordinate.column).set(coordinate.row, kanji);
+    }
+
+
+    public Kanji get(int column, int row) throws InvalidCoordinateException{
+        Coordinate coordinate = processCoordinates(column, row);
+        return this.board.get(coordinate.column).get(coordinate.row);
+    }
+
+    private static class Coordinate{
+        private int column;
+        private int row;
+
+        private Coordinate(int column, int row) {
+            this.column = column;
+            this.row = row;
+        }
+
+        private void standardiseIndices() {
+            this.column--;
+            this.row--;
+        }
+
+    }
 
 }
